034358a4fbcd31cd1e20a90b53c359f7a20f47c4
Dow Draper <dow.h.draper@gmail.com>
2022-11-17 15:27:38 -0500

    Reorganize the data to a subsection of relevant data

--- main.py
+++ main.py
@@ -3,25 +3,22 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
-    # TODO: ABCDEFG
-    hd = ["Origin", "Destination", "Origin City", "Destination City", "Passengers", "Seats", "Flights", "Distance", "Fly Date",
-          "Origin Population", "Destination Population"]
+    hd = ["Origin", "Destination", "Origin City", "Destination City", "Passengers", "Seats", "Flights", "Distance",
+          "Fly Date", "Origin Population", "Destination Population"]
     dat = pd.read_csv(r"C:\Users\Dow\Downloads\chimps_16091-2010-08-03_17-08-31\flight_edges.tsv", sep="\t", names=hd)
+    dat2 = dat[dat["Destination"] == "MFR"]
+
+    for i in dat2.columns:
+        if i != "Passengers" and i != "Fly Date" and i != "Flights":
+            del dat2[i]
+
+    dat2.loc[:, "Fly Date"] = pd.to_datetime(dat2.loc[:, "Fly Date"].copy(), format="%Y%m")
+
+    dat2.set_index("Fly Date", inplace=True)
+    dat2 = dat2.sort_index()
+    print("MFR Month-Passenger")
+    print(dat2.head(10))
+    print(dat2.groupby("Fly Date").sum().head(10))
 
 
-    #print(dat["Passengers"][:20])
-    #dat["Test"] = dat["Passengers"].rolling(window=10, min_periods=10).mean()
-
-    #print(dat[(dat["Passengers"] != 0) & (dat["Seats"] == 0)]["Passengers"])
-    #print(dat.loc[1695204,["Origin", "Destination"]])
-    #print(len(dat[dat["Seats"] == 0]))
-    #print(len(dat[(dat["Passengers"] == 0) & (dat["Seats"] != 0)]))
-    #print(len(dat[dat['Passengers'].equals(dat['Seats'])]))
-    #print(dat["Destination"])
-    #print(dat.info())
-    #print(dat["Test"][:20])
-    #dat["Distance", "Destination Population"]
-    test = dat.head(20)
-    test.plot.scatter(x='Passengers', y='Origin',  c='Red')
-    plt.show()
 
